40bccf6e173752352d382dd8e0c6e5b9ea433d9f
artemisia000 <misiapierobon@gmail.com>
2021-11-11 15:19:22 +0100

    title e message

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+
+const app = new Vue ({
+
+    el: '#root',
+
+    data: {
+
+        title: 'BANKSY',
+        message: '“La gente dice che i graffiti sono brutti, irresponsabili e infantili. Ma questo è solo se sono fatti correttamente. ”',
+        image:  'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.ilmessaggerocasa.it%2Fnews%2Fcasa-news%2Fbansky_graffito_venezia_valore_immobile_palazzo%2F4514928.html&psig=AOvVaw1H7gzdnCwxGoeov4HJEXbr&ust=1636725088932000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCPDzhq26kPQCFQAAAAAdAAAAABAD',
+    } ,
+});
